ab9d24e955abf671c0edad1f55cdec9e0bb351bb
Brandon Cho <hjun1114@gmail.com>
2017-04-27 11:08:46 -0400

    adding search bar

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.use(express.static("assets"));
 app.set("view engine", "ejs");
 
-// app.get("/", function(req, res){
-//   res.render("home");
-// });
+
 app.get("/results", function(req, res){
-  request("https://yjm3zctbgh.execute-api.us-east-1.amazonaws.com/productionV1/products", function(error, response, body){
+  var query = req.query.search;
+  var url = "https://yjm3zctbgh.execute-api.us-east-1.amazonaws.com/productionV1/products?=" + query ;
+  // What I tried to do is to fetch the data that are only realted to the searched products
+  request(url, function(error, response, body){
     if(!error && response.statusCode == 200) {
-      var parsedData = JSON.parse(body) // we get the body object as string, so have to parse it and store in a variable
-      res.render("results", {parsedData: parsedData});
+      var parsedData = JSON.parse(body)
     }
   });
+});
+app.get("/", function(req, res){
+  res.render("search");
 });
 app.listen(3000, function() {
   console.log('App has started on port 3000');
